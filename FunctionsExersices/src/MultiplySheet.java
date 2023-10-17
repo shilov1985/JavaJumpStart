4d94b4fbe511f2975c75bfd8d6fbafc1626f54bd
Miroslav Shilov <miroslavshilov@gmail.com>
2023-10-17 10:25:04 +0300

    Replace for loop with 'if' condition.

--- FunctionsExersices/src/MultiplySheet.java
+++ FunctionsExersices/src/MultiplySheet.java
@@ -1,20 +1,22 @@
 public class MultiplySheet {
     public static void main(String[] args) {
-        printMultiplySheet(1);
+        printMultiplySheet(1, 0);
     }
 
-    private static void printMultiplySheet(int number) {
-        int result;
-        if (number > 10) {
+    private static void printMultiplySheet(int number1, int number2) {
+        if (number2 > 10) {
+            number2 = 0;
+            number1++;
+        }
+        if (number1 > 10) {
             return;
         }
 
-        for (int i = 0; i <= 10; i++) {
-            result = number * i;
-            System.out.println(number + "  X  " + i + "  =  " + result);
-        }
-        number++;
-        printMultiplySheet(number);
+        int result = number1 * number2;
+        System.out.println(number1 + "  X  " + number2 + "  =  " + result);
+
+        number2++;
+        printMultiplySheet(number1, number2);
 
     }
 }
